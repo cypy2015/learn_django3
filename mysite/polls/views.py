c359f447b652774c85ef77e852d62c96b8679b8b
cypy2015 <157226691@qq.com>
2021-02-25 09:49:27 +0800

    test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
@@ -13,9 +16,9 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse('you are looking at resualt of question %s' % question_id)
+    return HttpResponse('you are looking at result of question %s' % question_id)
 
 
-def vote(request,question_id):
+def vote(request, question_id):
     return HttpResponse('you are voting on question %s' % question_id)
 
