19bb1417421712151baa3b7baa71389c3c009b17
cypy2015 <157226691@qq.com>
2021-02-25 09:18:52 +0800

    test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('Hello world')
+
+
+def detail(request, question_id):
+    return HttpResponse('you are looking at question %s' % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse('you are looking at resualt of question %s' % question_id)
+
+
+def vote(request,question_id):
+    return HttpResponse('you are voting on question %s' % question_id)
+
